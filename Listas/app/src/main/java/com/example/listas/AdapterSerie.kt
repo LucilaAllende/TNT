52c1947969d752b5b0a3cba39cb307b10d03bb9c
LuluAllende19 <abigail_lucii@hotmail.com>
2020-06-14 15:16:46 -0300

    Ver Detalle serie

--- Listas/app/src/main/java/com/example/listas/AdapterSerie.kt
+++ Listas/app/src/main/java/com/example/listas/AdapterSerie.kt
@@ -1,21 +1,30 @@
 package com.example.listas
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.content_item.view.*
+
 
 //clase adaptador para recibir los datos
 class AdapterSerie (var list: ArrayList<Serie>): RecyclerView.Adapter<AdapterSerie.ViewHolder>(){
     //clase para manejar nuestra vista
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view){ //el view que vamos agregar dentro de este es el view que recibimos en la clase viewHolder
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener { //el view que vamos agregar dentro de este es el view que recibimos en la clase viewHolder
+
+        private val btnVer: Button = view.findViewById(R.id.btnVer)
+        private val context: Context = view.context
 
         //recibimos lo datos que se agregan dentro de nuestra vista
         fun bindItems (data: Serie){
+
             //variables para nuestras vistas
             val title: TextView = itemView.findViewById(R.id.txtTitle)
             val tempo: TextView = itemView.findViewById(R.id.txtTemporadas)
@@ -31,10 +40,24 @@ class AdapterSerie (var list: ArrayList<Serie>): RecyclerView.Adapter<AdapterSer
             }
 
         }
+
+        fun escuchame(){
+            btnVer.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            when (v?.id) {
+                R.id.btnVer -> {
+                    val intent = Intent(context, DetallesSerie::class.java)
+                    intent.putExtra("title", itemView.txtTitle.text);
+                    context.startActivity(intent)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var v = LayoutInflater.from(parent?.context).inflate(R.layout.content_item, parent, false)
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.content_item, parent, false)
         return ViewHolder(v)
     }
 
@@ -42,7 +65,10 @@ class AdapterSerie (var list: ArrayList<Serie>): RecyclerView.Adapter<AdapterSer
         return list.size
     }
 
-    override fun onBindViewHolder(holder: AdapterSerie.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(list[position])
+
+        //set eventos
+        holder.escuchame()
     }
 }
--- Listas/app/src/main/java/com/example/listas/DetallesSerie.kt
+++ Listas/app/src/main/java/com/example/listas/DetallesSerie.kt
@@ -1,12 +1,21 @@
 package com.example.listas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class DetallesSerie : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalles_serie)
+
+        val extras: Bundle? = intent?.extras
+        val txtDetalle: TextView? = findViewById(R.id.txtDetalle)
+        val datos = extras?.getString("title")
+
+        if (extras != null) {
+            txtDetalle?.text= datos
+        }
     }
 }
