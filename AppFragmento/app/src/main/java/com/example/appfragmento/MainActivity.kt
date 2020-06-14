5663fc1c7929822065eec850fc566bfc6b4af64e
LuluAllende19 <abigail_lucii@hotmail.com>
2020-06-14 19:49:46 -0300

    Comunicacion actividad fragmento

---
+++ AppFragmento/app/src/main/java/com/example/appfragmento/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.appfragmento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnFragmentActionsListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnRed.setOnClickListener { replaceFragment(RedFragment()) }
+        btnBlue.setOnClickListener { replaceFragment(BlueFragment()) }
+    }
+
+    override fun onClickFragmentButton() {
+        Toast.makeText(this, "El botón ha sido pulsado", Toast.LENGTH_SHORT).show()
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+}
---
+++ AppFragmento/app/src/main/java/com/example/appfragmento/RedFragment.kt
@@ -0,0 +1,43 @@
+package com.example.appfragmento
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_red.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RedFragment : Fragment() {
+
+    private var listener: OnFragmentActionsListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blue, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnPlus?.setOnClickListener { listener?.onClickFragmentButton()}
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentActionsListener) {
+            listener = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+}
